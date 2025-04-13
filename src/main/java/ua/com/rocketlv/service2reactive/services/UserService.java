6fb6961aed7cec7c365b536cfdbf3c07e6d9806a
rocketlv <rocketlv@gmail.com>
2025-04-13 13:05:38 +0300

    add creational method

--- src/main/java/ua/com/rocketlv/service2reactive/services/UserService.java
+++ src/main/java/ua/com/rocketlv/service2reactive/services/UserService.java
@@ -2,15 +2,19 @@ package ua.com.rocketlv.service2reactive.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import ua.com.rocketlv.service2reactive.dao.User;
+import ua.com.rocketlv.service2reactive.dao.Userlog;
+import ua.com.rocketlv.service2reactive.dto.UserDto;
 import ua.com.rocketlv.service2reactive.exceptions.ObjectNotFoundException;
 import ua.com.rocketlv.service2reactive.exceptions.UserNotFoundException;
-import ua.com.rocketlv.service2reactive.dto.UserDto;
 import ua.com.rocketlv.service2reactive.repo.UserRepository;
 import ua.com.rocketlv.service2reactive.repo.UserlogRepository;
 
 import java.time.Duration;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -21,16 +25,16 @@ public class UserService {
     private final UserlogRepository userlogRepository;
 
     public Mono<UserDto> getUserById(Long id) {
-        var s =  userRepository.findById(id).map(userMapper::mapToUserDto).filter(userDto -> userDto.getId().equals(id))
+        var s = userRepository.findById(id).map(userMapper::mapToUserDto).filter(userDto -> userDto.getId().equals(id))
                 .switchIfEmpty(Mono.error(new UserNotFoundException(id)));
-        s.subscribe(res-> System.out.println("User found: " + res));
+        s.subscribe(res -> System.out.println("User found: " + res));
         return s;
 
     }
 
     public Flux<UserDto> getAllUsers() {
-        var u =  userRepository.findAll().map(userMapper::mapToUserDto).delayElements(Duration.ofMillis(0));
-        u.subscribe(res-> System.out.println("All users: " + res));
+        var u = userRepository.findAll().map(userMapper::mapToUserDto).delayElements(Duration.ofMillis(0));
+        u.subscribe(res -> System.out.println("All users: " + res));
         return u;
     }
 
@@ -40,12 +44,42 @@ public class UserService {
         );
     }
 
+//    public Mono<UserDto> getUserWithLogs(Long id) {
+//        return userRepository.findById(id)
+//                .flatMap(user -> userlogRepository.findByUserId(id)
+//                        .collectList()
+//                        .map(logs -> userMapper.mapToUserDtoWithLogs(user, logs)))
+//                .switchIfEmpty(Mono.error(new UserNotFoundException(id)));
+//    }
+
     public Mono<UserDto> getUserWithLogs(Long id) {
-        return userRepository.findById(id)
-                .flatMap(user -> userlogRepository.findByUserId(id)
-                        .collectList()
-                        .map(logs -> userMapper.mapToUserDtoWithLogs(user, logs)))
-                .switchIfEmpty(Mono.error(new UserNotFoundException(id)));
+
+        var userDtoMono = userRepository.findById(id)
+                .switchIfEmpty(Mono.error(new UserNotFoundException(id)))
+                .flatMap(user ->
+                        userlogRepository.findByUserId(user.getId())
+                                .collectList()
+                                .map(logs -> userMapper.mapToUserDtoWithLogs(user, logs)));
+        return userDtoMono;
     }
 
+    @Transactional(rollbackFor = RuntimeException.class)
+    public Mono<UserDto> createUserWithLogs(UserDto userDto, List<Userlog> userlogs) {
+        return Mono.just(userDto)
+                .filter(dto -> dto != null)
+                .map(userMapper::mapToUser)
+                .flatMap(user -> userRepository.save(user)
+                        .switchIfEmpty(Mono.error(new RuntimeException("User creation Error !")))
+                        .flatMap(createduser -> {
+                            Flux<Userlog> userlogFlux = Flux.fromIterable(userlogs);
+                            return userlogFlux.filter(listoflogs -> listoflogs != null)
+                                    .flatMap(userlog -> {
+                                        userlog.setUserId(createduser.getId());
+                                        return userlogRepository.save(userlog)
+                                                .switchIfEmpty(Mono.error(new RuntimeException("User log creation Error !")));
+                                    }).then(Mono.just(createduser).map(userMapper::mapToUserDto));
+                        })
+                );
+
+    }
 }
